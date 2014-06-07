481238ed061bb8e0e5bcceb60ba896b5f459997d
Eric Douglas <eric.eoop@gmail.com>
2014-06-07 20:54:55 -0300

    add carbrand first functionality - screencast

--- sections/screencasts/building-db-app/carbrands/carbrands.js
+++ sections/screencasts/building-db-app/carbrands/carbrands.js
@@ -1,19 +1,43 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to carbrands.";
-  };
+var okCancelEvents = function( selector, callbacks ) {
+  var ok = callbacks.ok || function() {};
+  var cancel = callbacks.cancel || function() {};
 
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+  var events = {};
+  events[ 'keyup ' + selector + ', keydown ' + selector + ', focusout ' + selector ] = 
+    function( evt ) {
+      if ( evt.type === 'keydown' && evt.switch === 27 ) {
+        // escape = cancel
+        cancel.call( this, evt );
+      }
+      else if ( evt.type === 'keyup' && evt.which === 13 ) {
+        // blur/return/enter = ok/submit if non-empty
+        var value = String( evt.target.value || '' );
+        if ( value )
+          ok.call( this, value, evt );
+        else
+          cancel.call( this, evt );
+      }
+    };
+
+  return events;
+};
+
+CarBrands = new Meteor.Collection( 'carbrands' );
+
+if (Meteor.isClient) {
+  Template.main.carbrands = function() {
+    return CarBrands.find();
+  }
+
+  Template.main.events(okCancelEvents(
+      '#brand', {
+        ok: function( text, evt ) {
+          alert( text );
+        }
+      }
+    ));
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+
 }
